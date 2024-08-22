4d9052e9815389f864097d1337eb012022d5ae36
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2024-08-22 17:01:43 +0000

    less safe, but no `Option`

--- src/best_time_to_buy_and_sell_stock.rs
+++ src/best_time_to_buy_and_sell_stock.rs
@@ -27,59 +27,24 @@ pub struct Solution;
 
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
-        let (_, _, diff) = prices
+        let (_, diff) = prices
             .iter()
-            .fold((None, None, None), |(buy, sell, diff), &current| {
-                match (buy, sell, diff) {
-                    (None, None, diff) => (Some(current), None, diff),
-                    (Some(buy), None, diff) => {
-                        if buy > current {
-                            (Some(current), None, diff)
+            .skip(1)
+            .fold((prices[0], 0), |(buy, diff), &current| {
+                if buy > current {
+                    (current, diff)
+                } else {
+                    (
+                        buy,
+                        if diff < current - buy {
+                            current - buy
                         } else {
-                            (
-                                Some(buy),
-                                Some(current),
-                                diff.map(|diff| {
-                                    if diff < current - buy {
-                                        current - buy
-                                    } else {
-                                        diff
-                                    }
-                                })
-                                .or(Some(current - buy)),
-                            )
-                        }
-                    }
-                    (None, Some(sell), diff) => {
-                        if sell > current {
-                            (None, Some(sell), diff)
-                        } else {
-                            (Some(sell), Some(current), Some(current - sell))
-                        }
-                    }
-                    (Some(buy), Some(sell), diff) => {
-                        if buy > current {
-                            (Some(current), None, diff)
-                        } else if sell < current {
-                            (
-                                Some(buy),
-                                Some(current),
-                                diff.map(|diff| {
-                                    if diff < current - buy {
-                                        current - buy
-                                    } else {
-                                        diff
-                                    }
-                                })
-                                .or(Some(current - buy)),
-                            )
-                        } else {
-                            (Some(buy), Some(sell), diff)
-                        }
-                    }
+                            diff
+                        },
+                    )
                 }
             });
-        diff.unwrap_or(0)
+        diff.max(0)
     }
 }
 
